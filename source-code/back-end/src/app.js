3cf5c3bbd61b93765b7d2b68846e2d944dac5e83
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-06 16:31:25 -0500

    Added repository

--- source-code/back-end/src/app.js
+++ source-code/back-end/src/app.js
@@ -1,8 +1,10 @@
+require('dotenv').config(); // You should place this as early as possible in any source file that reads from process.env
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on http://localhost:${process.env.PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
