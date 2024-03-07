c2289e7ed2a6ac24ffda5570ada6cc9c354f7470
Ghabrille Ampo <75871616+ghabrille01@users.noreply.github.com>
2024-03-07 14:13:39 -0800

    merged front-end and back-end, added cors to back-end

--- source-code/back-end/src/app.ts
+++ source-code/back-end/src/app.ts
@@ -1,20 +1,25 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 /* For any source file that accesses process.env,
 import and call config() on dotenv as early as possible*/
 
-import express, { Express, Request, Response } from 'express';
-import recipeRouter from './controller/recipeRouter';
+const cors = require("cors");
+import express, { Express, Request, Response } from "express";
+import recipeRouter from "./controller/recipeRouter";
+import userRouter from "./controller/userRouter";
 
 const app: Express = express();
 const PORT = process.env.PORT;
 
+app.use(cors())
 app.use(express.json());
-app.use('/recipes', recipeRouter);
-app.get('/', (req: Request, res: Response) => {
-	res.send('The dude abides');
+
+app.use("/recipes", recipeRouter);
+app.use("/user", userRouter);
+app.get("/", (req: Request, res: Response) => {
+  res.send("The dude abides");
 });
 
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}.`);
+  console.log(`Server listening on port ${PORT}.`);
 });
