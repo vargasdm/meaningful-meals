e5face96e0f9d6da8fe2cd2f50a0c716c12e01d2
vargasdm <112888008+vargasdm@users.noreply.github.com>
2024-03-07 16:47:10 -0500

    added frontend register/login components

---
+++ source-code/front-end/src/components/Login/LoginContainer.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userActions } from "../../store/slices/userSlice";
+import LoginInput from "./LoginInput";
+import axios from "axios";
+const PORT = process.env.REACT_APP_PORT;
+const URL = `http://localhost:${PORT}`;
+
+function LoginContainer() {
+  const dispatch = useDispatch();
+
+  async function updateUser(user: any) {
+    // console.log(user);
+    console.log(dispatch(userActions.setUser(user)));
+    try {
+      // sends post request to backend
+      if (await getUser(user)) {
+        console.log("success");
+        // should change the global user state variable using the properties of the user object
+        dispatch(userActions.setUser(user));
+        console.log(dispatch(userActions.setUser(user)));
+        
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getUser(user: any) {
+    try {
+      let response = await axios.post(`${URL}/login`, {
+        username: user.username,
+        password: user.password,
+      });
+
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <LoginInput updateUser={updateUser} />
+    </>
+  );
+}
+
+export default LoginContainer;
---
+++ source-code/front-end/src/components/Register/RegisterInput.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function RegisterInput(props: any) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleRegisterSubmit(event: any) {
+    event.preventDefault();
+    props.updateUser({ username, password, email });
+  }
+
+  return (
+    <>
+      <h1>Register Form</h1>
+      <form onSubmit={handleRegisterSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          minLength={3}
+          maxLength={30}
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          minLength={3}
+          maxLength={30}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          minLength={3}
+          maxLength={30}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+      </form>
+    </>
+  );
+}
+
+export default RegisterInput;
