cf12592b31bd4e4ff1350a9b58d97884c2fe3537
Ghabrille Ampo <75871616+ghabrille01@users.noreply.github.com>
2024-03-07 13:25:51 -0800

    added login and registration for back-end without tests

---
+++ source-code/back-end/src/controller/userRouter.ts
@@ -0,0 +1,61 @@
+// endpoint: /user
+require("dotenv").config();
+import express from "express";
+import jwt from "jsonwebtoken";
+const router = express.Router();
+import { logger } from "../util/logger";
+import {
+  validateLoginBody,
+  validateRegisterBody,
+} from "../util/authenticateBody";
+import { authenticateNoToken } from "../util/authenticateToken";
+
+import { postEmployee, login } from "../service/userService";
+
+// Read
+router.post(
+  "/login",
+  authenticateNoToken,
+  validateLoginBody,
+  async (req: any, res: any) => {
+    const data: any = await login(req.body);
+    if (data) {
+      const token = jwt.sign(
+        {
+          user_id: data.user_id,
+          username: data.username,
+          role: data.role,
+        },
+        process.env.JWT_KEY as string,
+        {
+          expiresIn: "15m",
+        }
+      );
+
+      logger.info(`Login: ${data.username} Token: ${token}`);
+      res.status(201).json({ message: `Login: ${data.username}`, token });
+    } else {
+      res.status(400).json({ message: "Failed login" });
+    }
+  }
+);
+
+// Create
+router.post(
+  "/register",
+  authenticateNoToken,
+  validateRegisterBody,
+  async (req: any, res: any) => {
+    const data = await postEmployee(req.body);
+    if (data) {
+      logger.info(`Created Employee: ${data.username}`);
+      res.status(201).json({ message: `Created Employee ${data.username}` });
+    } else {
+      res.status(400).json({
+        message: "Employee was not created. Invalid Credentials.",
+      });
+    }
+  }
+);
+
+export default router;
---
+++ source-code/back-end/src/service/userService.ts
@@ -0,0 +1,41 @@
+import userDao from "../repository/userDAO";
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
+
+async function login(recievedData: any) {
+  const data : any = await userDao.getEmployeeByUsername(recievedData.username);
+  if (
+    data &&
+    recievedData.username === data[0].username &&
+    (await bcrypt.compare(recievedData.password, data[0].password))
+  ) {
+    return data ? data[0] : null;
+  }
+
+  return null;
+}
+
+async function postEmployee(receivedData: any) {
+  if (await validateUsername(receivedData.username)) {
+    let data = await userDao.postEmployee({
+      user_id: uuid.v4(),
+      username: receivedData.username,
+      password: await bcrypt.hash(receivedData.password, 10),
+      role: "user",
+    });
+    return data ? data : null;
+  }
+
+  return null;
+}
+
+async function validateUsername(data: any) {
+  const isTaken : any = await userDao.getEmployeeByUsername(data);
+  if (isTaken.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export { postEmployee, login };
