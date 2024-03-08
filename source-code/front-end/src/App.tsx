953daf3ae9364a9a49e159963f44fe2764f04640
vargasdm <112888008+vargasdm@users.noreply.github.com>
2024-03-08 17:02:54 -0500

    added functional navbar and created simple search functionality

--- source-code/front-end/src/App.tsx
+++ source-code/front-end/src/App.tsx
@@ -1,31 +1,50 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import LoginContainer from "./components/Login/LoginContainer";
 import RegisterContainer from "./components/Register/RegisterContainer";
-import Recipe, { loader as recipeLoader } from './components/Recipe/Recipe';
-import Error from './components/Error';
+import Recipe, { loader as recipeLoader } from "./components/Recipe/Recipe";
+import SearchContainer from "./components/Search/SearchContainer";
+import Error from "./components/Error";
 import "./App.css";
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element:
-			<div className="App">
-				<LoginContainer />
-				<RegisterContainer />
-			</div>,
-		errorElement: <Error />
-	},
-	{
-		path: '/recipes/:id',
-		element: <Recipe />,
-		loader: recipeLoader
-	}
-	// errorElement: <Error />
-]);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <div className="App">
+//         <LoginContainer />
+//         <RegisterContainer />
+//       </div>
+//     ),
+//     errorElement: <Error />,
+//   },
+//   {
+//     path: "/recipes/:id",
+//     element: <Recipe />,
+//     loader: recipeLoader,
+//   },
+//   // errorElement: <Error />
+// ]);
 
 function App() {
-	return <RouterProvider router={router} />
+  return (
+    <>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          {/* need to create the search component */}
+          <Route path="/" element={<SearchContainer />} />
+          <Route path="/login" element={<LoginContainer />} />
+          <Route path="/register" element={<RegisterContainer />} />
+          <Route path="/recipes/:id" element={<Recipe />} loader={recipeLoader} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      {/* <RouterProvider router={router} /> */}
+    </>
+  );
 }
 
 export default App;
