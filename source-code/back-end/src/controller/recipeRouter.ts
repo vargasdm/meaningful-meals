f55279b7fdc278994bdac58d64adc0eda732427f
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-07 21:19:26 -0500

    Got info for a single recipe on the backend

--- source-code/back-end/src/controller/recipeRouter.ts
+++ source-code/back-end/src/controller/recipeRouter.ts
@@ -3,11 +3,17 @@ dotenv.config();
 
 import express from 'express';
 const router = express.Router();
-const recipeService = require('../service/recipeService.ts');
+import recipeService from '../service/recipeService';
+// const recipeService = require('../service/recipeService.ts');
 
 router.get('/:query', async (req: any, res: any) => {
 	const recipes = await recipeService.searchRecipes(req.params.query);
 	res.json(recipes);
+});
+
+router.get('/:id/information', async (req: any, res: any) => {
+	const data = await recipeService.getRecipe(req.params.id);
+	res.json(data.data);
 });
 
 export default router;
--- source-code/back-end/src/service/recipeService.ts
+++ source-code/back-end/src/service/recipeService.ts
@@ -5,13 +5,24 @@ dotenv.config();
 import axios from 'axios';
 const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY;
 
+// TODO: collate API search results with those from our own database
+// TODO: use Levenshtein distance for a simple search?
 async function searchRecipes(query: string) {
-	const result = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey='
-		+ SPOONACULAR_API_KEY + `&query=${query}&instructionsRequired=true`);
+	const result = await axios.get(`https://api.spoonacular.com/recipes/`
+		+ `complexSearch?apiKey=${SPOONACULAR_API_KEY}&query=${query}`
+		+ `&instructionsRequired=true`);
 	console.log(result);
 	return result.data;
 }
 
-export {
-	searchRecipes
+async function getRecipe(id: string) {
+	const result = await axios.get(`https://api.spoonacular.com/recipes/`
+		+ `${id}/information?apiKey=${SPOONACULAR_API_KEY}`);
+	console.log(result);
+	return result;
+}
+
+export default {
+	searchRecipes,
+	getRecipe
 }
