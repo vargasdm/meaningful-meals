484f3feb774fb2379160ac270f40652e9228ebc4
vargasdm <112888008+vargasdm@users.noreply.github.com>
2024-03-11 17:30:16 -0400

    added conditional redering for if logged in. Removed email input element from registerInput component

--- source-code/front-end/src/components/Navbar/Navbar.tsx
+++ source-code/front-end/src/components/Navbar/Navbar.tsx
@@ -1,21 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 // will change this when I use bootstrap
 import "./Navbar.css";
 
 function Navbar() {
+  const userState = useSelector((state: RootState) => state.user); // Access the user state from the global state
+
+  console.log(userState); // Log the current user state
+
   return (
     <nav>
       <Link to="/">MeaningFul Meals</Link>
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
+      {userState?.username && userState?.password ? (
+        <ul>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/recipes">Recipes</Link>
+          </li>
+          <li>
+            <Link to="/plan">Meal Plans</Link>
+          </li>
+          
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>test to see if conditional render works</li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
