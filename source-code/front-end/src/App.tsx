b9b48a0fdc0a77df4c0b9d5a0b206b1972ee5d80
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-12 11:45:05 -0400

    Migrating to new routers

--- source-code/front-end/src/App.tsx
+++ source-code/front-end/src/App.tsx
@@ -7,25 +7,26 @@ import Recipe, { loader as recipeLoader } from "./components/Recipe/Recipe";
 import SearchContainer from "./components/Search/SearchContainer";
 import Error from "./components/Error";
 import "./App.css";
-
+import { Outlet } from 'react-router-dom';
 
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        <Navbar />
-        <Routes>
+	return (
+		<>
+			<div className="App">
+				<Navbar />
+				{/* <Routes>
           <Route path="/" element={<SearchContainer />} />
           <Route path="/login" element={<LoginContainer />} />
           <Route path="/register" element={<RegisterContainer />} />
           <Route path="/recipes/:id" element={<Recipe />} loader={recipeLoader} />
           <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
-      {/* <RouterProvider router={router} /> */}
-    </>
-  );
+        </Routes> */}
+				<Outlet />
+			</div>
+			{/* <RouterProvider router={router} /> */}
+		</>
+	);
 }
 
 export default App;
--- source-code/front-end/src/index.tsx
+++ source-code/front-end/src/index.tsx
@@ -6,18 +6,52 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { BrowserRouter } from "react-router-dom";
+// import React from "react";
+import { createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import LoginContainer from "./components/Login/LoginContainer";
+import RegisterContainer from "./components/Register/RegisterContainer";
+import Recipe, { loader as recipeLoader } from "./components/Recipe/Recipe";
+import SearchContainer from "./components/Search/SearchContainer";
+import Error from "./components/Error"
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />, // This should be the root route
+		errorElement: <Error />,
+		children: [
+			{
+				path: '/',
+				element: <LoginContainer />
+			}, {
+				path: '/login',
+				element: <LoginContainer />
+			}, {
+				path: '/register',
+				element: <RegisterContainer />
+			}, {
+				path: '/recipes/:id',
+				element: <Recipe />,
+				loader: recipeLoader
+			}
+		]
+	}
+]);
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		{/* <BrowserRouter> */}
+		<Provider store={store}>
+			{/* <App /> */}
+			<RouterProvider router={router} />
+		</Provider>
+		{/* </BrowserRouter> */}
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
