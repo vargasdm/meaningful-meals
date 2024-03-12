2acd55fe9876c33149c6d8ef2460c5f742f3535f
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-12 19:11:21 -0400

    Commit before pulling Daniel's changes, 3/12/2024

--- source-code/front-end/src/store/slices/userSlice.tsx
+++ source-code/front-end/src/store/slices/userSlice.tsx
@@ -1,17 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        username: "",
-        password: ""
-    },
-    reducers: {
-        setUser(state, param){
-            state.username = param.payload.username;
-            state.password = param.payload.password;
-        }
-    }
+	name: "user",
+	initialState: {
+		user_id: "",
+		username: "",
+		isAuthenticated: false
+		// password: "" We don't want to store the password client-side!!
+	},
+	reducers: {
+		// setUser(state, action){
+		// state.username = action.payload.username;
+		// state.password = param.payload.password;
+		// }
+		setUser: (state, action) => {
+			state = {...state, ...(action.payload)}
+			// This should set state to an object literal that was first
+			// cloned from the original state, then had any properties that
+			// shared the same name as those in action.payload overwritten
+			// by those in action.payload
+			// TODO: unit-test this!
+		}
+	}
 })
 
-export const userActions = userSlice.actions
+// export const userActions = userSlice.actions
+export const {setUser} = userSlice.actions;
