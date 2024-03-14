4dd067cb222f79710aae5c17f00669cc0641b909
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-14 17:55:24 -0400

    Persisting Redux state, at least for Profile

--- source-code/front-end/src/components/Profile/Profile.tsx
+++ source-code/front-end/src/components/Profile/Profile.tsx
@@ -3,21 +3,14 @@ import { useLoaderData } from 'react-router-dom';
 import store from '../../store/store';
 
 export async function loader() {
-	// const user = useSelector((state: any) => state.user);
-	// const user = store.getState().user;
-	// console.log(`Profile.tsx user: ${JSON.stringify(user)}`);
-	// return null;
-	return store.getState().user;
+	// return store.getState().user;
 	// GLOBAL STATE DOESN'T REHYDRATE TIL AFTER LOADERS RUN
 	// DON'T ACCESS GLOBAL STATE IN THE LOADER
+	return null;
 }
 
 export default function Profile() {
-	// const data: any = useLoaderData();
-	// console.log(store.getState());
 	const globalState = store.getState();
-	// INSTEAD, ACCESS GLOBAL STATE IN THE COMPONENT ITSELF
-	console.log(globalState);
 
 	return (
 		<h1>{globalState.user.username}</h1>
