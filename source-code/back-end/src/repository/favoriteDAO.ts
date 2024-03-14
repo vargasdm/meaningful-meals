f2ca4fe81f008416e1097554761d8e4dab801e07
Ghabrille Ampo <75871616+ghabrille01@users.noreply.github.com>
2024-03-14 16:49:36 -0700

    added favorite: router and service

--- source-code/back-end/src/repository/favoriteDAO.ts
+++ source-code/back-end/src/repository/favoriteDAO.ts
@@ -1,3 +1,14 @@
+require("dotenv").config();
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+	DynamoDBDocumentClient,
+	PutCommand,
+	QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { logger } from "../util/logger";
 
+const client = new DynamoDBClient({ region: process.env.AWS_REGION as string });
+const documentClient = DynamoDBDocumentClient.from(client);
+const TableName: string = process.env.USERS_TABLE as string;
 
-export {}
+export default {}
