969c832f7394ed8d414ff45b0a17718077ff3295
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-15 18:18:07 -0400

    Added 'Add to Meal Plan' button to recipe page'

--- source-code/front-end/src/components/Recipe/Recipe.tsx
+++ source-code/front-end/src/components/Recipe/Recipe.tsx
@@ -1,37 +1,42 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import './Recipe.css';
+
 const PORT = process.env.REACT_APP_PORT;
 const URL = `http://localhost:${PORT}/recipes`;
 export async function loader({ params }: any) {
-  try {
-    const res = await axios.get(`${URL}?id=${params.id}`);
+	try {
+		const res = await axios.get(`${URL}?id=${params.id}`);
 
-    return res.data;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
+		return res.data;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
 }
 
 export default function Recipe() {
-  const data: any = useLoaderData();
-  console.log(data);
+	const data: any = useLoaderData();
+	console.log(data);
 
-  const instructions: any = data.analyzedInstructions[0].steps.map(
-    (step: any) => <li key={step.number}>{step.step}</li>
-  );
-  const ingredients: any = data.nutrition.ingredients.map(
-    (ingridients: any) => <li key={ingridients.id}>{ingridients.name}</li>
-  );
+	const instructions: any = data.analyzedInstructions[0].steps.map(
+		(step: any) => <li key={step.number}>{step.step}</li>
+	);
+	const ingredients: any = data.nutrition.ingredients.map(
+		(ingredient: any) => <li key={ingredient.id}>{ingredient.name}</li>
+	);
 
-  return (
-    <>
-      <h1>{data.title}</h1>
-      <img src={data.image} alt={data.title} />
-      <h2>Ingredients</h2>
-      <ul>{ingredients}</ul>
-      <h2>Instructions</h2>
-      <ol>{instructions}</ol>
-    </>
-  );
+	return (
+		<div className='recipe-div'>
+			<div className='recipe-main-header'>
+				<h1>{data.title}</h1>
+				<input type='button' value='Add to Meal Plan' />
+			</div>
+			<img src={data.image} alt={data.title} />
+			<h2>Ingredients</h2>
+			<ul>{ingredients}</ul>
+			<h2>Instructions</h2>
+			<ol>{instructions}</ol>
+		</div>
+	);
 }
