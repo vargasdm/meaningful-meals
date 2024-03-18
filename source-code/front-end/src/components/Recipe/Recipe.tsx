c423ea200d2763e77118e80a9632cde9fd38bf73
Langchun Zhou <zhoulangchun@gmail.com>
2024-03-18 02:29:51 -0400

    'Add to meal plan' button checking whether associated recipe is in meal plan

--- source-code/front-end/src/components/Recipe/Recipe.tsx
+++ source-code/front-end/src/components/Recipe/Recipe.tsx
@@ -1,46 +1,90 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import store from "../../store/store";
 import './Recipe.css';
 import endpoints from '../../endpoints';
 
 const BACKEND_PORT = process.env.REACT_APP_PORT;
-const RECIPES_ENDPOINT = 
+const RECIPES_ENDPOINT =
 	endpoints.RECIPES_ENDPOINT || `http://localhost:${BACKEND_PORT}/recipes`;
 const MEALS_ENDPOINT = endpoints.MEALS_ENDPOINT;
 
-// console.log(MEALS_ENDPOINT);
+console.log(MEALS_ENDPOINT);
 
 export async function loader({ params }: any) {
 	try {
-		const res = await axios.get(`${RECIPES_ENDPOINT}?id=${params.id}`);
-
-		return res.data;
+		// const isUserMeal = await axios.get(
+		// 	`${MEALS_ENDPOINT}/params.id`,
+		// 	{
+		// 		headers: {
+		// 			'Authorization': 'Bearer'
+		// 		}
+		// 	}
+		// )
+		// console.log(store.getState());
+		const recipeData = await axios.get(`${RECIPES_ENDPOINT}?id=${params.id}`);
+		return recipeData.data;
 	} catch (err) {
 		console.error(err);
 		return null;
 	}
 }
 
-export default function Recipe() {
-	const data: any = useLoaderData();
-	console.log(data);
 
-	const instructions: any = data.analyzedInstructions[0].steps.map(
+export default function Recipe() {
+
+	const [isInMealPlan, setIsInMealPlan] = useState(false);
+	const recipeData: any = useLoaderData();
+	console.log(recipeData);
+
+	const user = useSelector((state: any) => state.user);
+	const jwt = user.jwt;
+	// isMeal();
+	// isInMealPlan();
+	async function getIsInMealPlan(): Promise<void> {
+		// console.log(store.getState());
+		const jwt = store.getState().user.jwt;
+
+		try {
+			const meal = await axios.get(
+				`${MEALS_ENDPOINT}/params.id`,
+				{
+					headers: {
+						'Authorization': `Bearer ${jwt}`
+					}
+				}
+			);
+
+			// console.log(meal);
+			setIsInMealPlan(meal ? true : false);
+		} catch (err) {
+			console.error(err);
+			setIsInMealPlan(false);
+		}
+	}
+
+	useEffect(() => {
+		getIsInMealPlan();
+	}, [])
+
+	const instructions: any = recipeData.analyzedInstructions[0].steps.map(
 		(step: any) => <li key={step.number}>{step.step}</li>
 	);
-	const ingredients: any = data.nutrition.ingredients.map(
+	const ingredients: any = recipeData.nutrition.ingredients.map(
 		(ingredient: any) => <li key={ingredient.id}>{ingredient.name}</li>
 	);
 
 	return (
 		<div className='recipe-div'>
 			<div className='recipe-main-header'>
-				<h1>{data.title}</h1>
+				<h1>{recipeData.title}</h1>
 				<input
 					type='button'
-					value='Add to Meal Plan' />
+					value={isInMealPlan ? 'Remove from Meal Plan' : 'Add to Meal Plan'} />
 			</div>
-			<img src={data.image} alt={data.title} />
+			<img src={recipeData.image} alt={recipeData.title} />
 			<h2>Ingredients</h2>
 			<ul>{ingredients}</ul>
 			<h2>Instructions</h2>
